df9a75f8dbcc5cd86fff7348c6ce09e59dbe0c9f
camilo1reis <camiloreis.jf@gmail.com>
2023-07-01 23:25:08 -0300

    feat: Adição de funcionalidades, refatoração.

--- src/test/java/TesteConexaoBD.java
+++ src/test/java/TesteConexaoBD.java
@@ -5,7 +5,20 @@ import javax.persistence.Persistence;
 
 public class TesteConexaoBD {
     public static void main(String[] args) {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("sistemaAvaliacaoPU");
-        entityManagerFactory.close();
+        EntityManagerFactory entityManagerFactory = null;
+        try {
+            entityManagerFactory = Persistence.createEntityManagerFactory("sistemaAvaliacaoPU");
+            System.out.println("Conexão com o banco de dados estabelecida com sucesso!");
+        } catch (Exception e) {
+            System.out.println("Erro ao conectar ao banco de dados: " + e.getMessage());
+        } finally {
+            if (entityManagerFactory != null) {
+                entityManagerFactory.close();
+            }
+        }
     }
+//    public static void main(String[] args) {
+//        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("sistemaAvaliacaoPU");
+//        entityManagerFactory.close();
+//    }
 }
